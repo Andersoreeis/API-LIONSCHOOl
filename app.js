f1958d225a2f7d0707ec50ace6b32aefefc9ff28
ANDERSON REIS GONÇALVES <22282173@CFP127ALUNO.LOCAL>
2023-03-27 16:58:45 -0300

    Terminando

---
+++ app.js
@@ -0,0 +1,149 @@
+/***********************************************************************************
+ *Objetivo: Criar uma API para disponibilizar nome de cursos, nome de alunos, foto de alunos, e as notas em cada disciplina do aluno especificado
+ *Autor: Anderson Reis
+ *Versão: 1.0
+ *Data 27/03/2023
+ ************************************************************************************/
+
+const express = require('express'); // dependecia para criar as aquisções da API
+const cors = require('cors'); // dependecia para gerenciar as permissões
+const bodyParser = require('body-parser'); // dependencia para gerenciar o corpo das requisições da API
+const alunos = require('./alunos.js')
+const cursos = require('./cursos.js')
+const getsCursosEAlunos = require('./main.js')
+
+const app = express(); // cria objeto com as características do express
+app.use((request, response, next) => {
+    //API pública, que todo mundo pode usar
+    //API privada
+    response.header('Access-Control-Allow-Origin', '*'); // * significa que fica publico ou seja todo mundo pode usar
+    //response.header('Access-Control-Allow-Methods','ip do seu servidor'); // ip quando é privada só podera usar api a pessoa com o ip colocado
+    response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS'); // permite definir quais métodos poderão ser utlizados nas requisições da API
+
+
+    app.use(cors()) // Envia para o cors() as regras de permissões
+
+    next()
+})
+
+//EndPoints
+app.get('/v1/lion-school/cursos', cors(), async function (request, response, next){
+let cursosResponse =  getsCursosEAlunos.getCursos()
+if(cursosResponse){
+    response.status(200)
+    response.json(cursosResponse)
+}
+else{
+    response.status(500)
+}
+})
+
+app.get('/v1/lion-school/alunos', cors(), async function (request, response, next){
+    let alunosResponse = getsCursosEAlunos.getTodosAlunos()
+    if(alunosResponse){
+        response.status(200)
+        response.json(alunosResponse)
+    }else{
+        response.status(500)
+    }
+})
+app.get('/v1/lion-school/alunos/:matricula', cors(), async function (request, response, next){
+
+
+    let statusCode
+    let dadosAlunosMatriculados = {}
+    let siglaMatricula = request.params.matricula
+    if(siglaMatricula == '' || siglaMatricula == undefined || siglaMatricula == isNaN(siglaMatricula)){
+        statusCode = 400
+        dadosAlunosMatriculados.message = 'Não foi possivel processar pois os dados de entrada (matricula) não corresponde ao exígido, confira o valor pois não pode ser vázio, e precisa ser caracteres e ter dois digitos.'
+
+    }else{
+        let dadosAlunos = getsCursosEAlunos.getAlunosPelaMatricula(siglaMatricula)
+        if(dadosAlunos){
+            statusCode = 200
+            dadosAlunosMatriculados = dadosAlunos
+        }else{
+            statusCode = 400
+        }
+
+    }
+    response.status(statusCode)
+     response.json(dadosAlunosMatriculados)
+    
+    
+
+})
+app.get('/v1/lion-school/alunos/:curso',  cors(), async function (request, response, next){
+
+    let statusCode
+    let dadosAlunosPeloCurso = {}
+    let siglaCurso = request.query.curso
+    if(siglaCurso == '' || siglaCurso == undefined || siglaCurso == !isNaN(siglaCurso)){
+        statusCode = 400
+        dadosAlunosMatriculados.message = 'Não foi possivel processar pois os dados de entrada (sigla do curso) não corresponde ao exígido, confira o valor pois não pode ser vázio, e precisa ser caracteres e ter dois digitos.'
+
+    }else{
+        let dadosAluno = getsCursosEAlunos.getAlunosPeloCurso(siglaCurso)
+        if(dadosAluno){
+            statusCode = 200
+            dadosAlunosPeloCurso = dadosAluno
+        }else{
+            statusCode = 400
+        }
+    }
+    response.status(statusCode)
+    response.json(dadosAlunosPeloCurso)
+})
+
+
+app.get('/v1/lion-school/alunos/:status',  cors(), async function (request, response, next){
+
+    let statusCode
+    let dadosAlunosPeloStatus = {}
+    let situacao = request.query.status
+    if(situacao == '' || situacao == undefined || situacao == !isNaN(situacao)){
+        statusCode = 400
+        dadosAlunosPeloStatus.message = 'Não foi possivel processar pois os dados de entrada (status) não corresponde ao exígido, confira o valor pois não pode ser vázio, e precisa ser caracteres e ter dois digitos.'
+
+    }else{
+        let dadosAluno = getsCursosEAlunos.getAlunosStatus(situacao)
+        if(dadosAluno){
+            statusCode = 200
+            dadosAlunosPeloStatus = dadosAluno
+        }else{
+            statusCode = 400
+        }
+    }
+    response.status(statusCode)
+    response.json(dadosAlunosPeloStatus)
+})
+
+
+app.get('/v1/lion-school/alunos/:matriculasigla',  cors(), async function (request, response, next){
+
+    let statusCode
+    let materiasAluno = {}
+    let matricula = request.query.matriculasigla
+    if(matricula == '' || matricula == undefined || matricula == isNaN(matricula)){
+        statusCode = 400
+        materiasAluno.message = 'Não foi possivel processar pois os dados de entrada (status) não corresponde ao exígido, confira o valor pois não pode ser vázio, e precisa ser caracteres e ter dois digitos.'
+
+    }else{
+        let dadosAluno = getsCursosEAlunos.getCursoSigla(matricula)
+        if(dadosAluno){
+            statusCode = 200
+            materiasAluno = dadosAluno
+        }else{
+            statusCode = 400
+        }
+    }
+    response.status(statusCode)
+    response.json(materiasAluno)
+})
+
+
+app.listen(8080, function () {
+    console.log(`Servidor aguardando requisições na porta 8080`);
+
+        
+})
--- main.js
+++ main.js
@@ -40,8 +40,7 @@ function getTodosAlunos() { // pega todos os alunos
 
 }
 
-
-function getAlunosPelaMatricula(matricula) {
+function getAlunosPelaMatricula(matricula) { //pega o aluno especifico pela matricla
     matricula.toUpperCase()
     let listaMatriculaArray = []
     let status
@@ -55,70 +54,117 @@ function getAlunosPelaMatricula(matricula) {
 
         status = true
     })
-    if(status){
+    if (status) {
         return listaMatriculaArray
-    }else{
+    } else {
         return status = false
     }
 }
 
-function getAlunosPeloCurso(cursoSigla){
+function getAlunosPeloCurso(cursoSigla) { // pega os alunos que estão em determinado curso
     cursoSigla.toUpperCase()
     let listaAlunosPeloCurso = []
+    let listaAlunosJSON = {}
     let status
-   
-    alunosJSON.forEach(function (pegarAlunos){
-        pegarAlunos.curso.forEach(function(pegarCursos){
-            if(pegarCursos.sigla == cursoSigla){
-                listaAlunosPeloCurso.push(pegarAlunos.nome)
-                listaAlunosPeloCurso.push(pegarAlunos.foto)
-                listaAlunosPeloCurso.push(pegarAlunos.matricula)
-                listaAlunosPeloCurso.push(pegarAlunos.sexo)
+
+    alunosJSON.forEach(function (pegarAlunos) {
+        pegarAlunos.curso.forEach(function (pegarCursos) {
+            if (pegarCursos.sigla == cursoSigla) {
+                // listaAlunosJSON.nome = pegarAlunos.nome
+                // listaAlunosJSON.foto = pegarAlunos.foto
+                // listaAlunosJSON.matricula = pegarAlunos.matricula
+                // listaAlunosJSON.sexo = pegarAlunos.sexo
+                
+                // listaAlunosPeloCurso.push(listaAlunosJSON.nome)
+                // listaAlunosPeloCurso.push(listaAlunosJSON.foto)
+                // listaAlunosPeloCurso.push(listaAlunosJSON.matricula)
+                // listaAlunosPeloCurso.push(listaAlunosJSON.sexo)
+
+
+
                 status = true
             }
         })
     })
 
-    if(status){
-        return listaAlunosPeloCurso
-    }else{
+    if (status) {
+        return listaAlunosJSON
+    } else {
         return status = false
     }
 }
+console.log(getAlunosPeloCurso('RDS'));
 
-function getAlunosStatus(situação){
+
+function getAlunosStatus(situação) { // pega o aluno que estão aprovado ou reprovado ou concluido
     let listaAlunosPelaSituação = []
     let status = false
     situação = situação.toLowerCase()
 
-    alunosJSON.forEach(function (pegarAlunos){
-        pegarAlunos.curso.forEach(function(pegarCursos){
-           pegarCursos.disciplinas.forEach(function(pegarStatus){
-             //  const statusDoAluno =  pegarStatus.toLowerCase()
-               if(typeof pegarStatus.status === 'string' && pegarStatus.status.toLowerCase() === situação){
-                listaAlunosPelaSituação.push(pegarAlunos.nome)
-                listaAlunosPelaSituação.push(pegarAlunos.foto)
-                listaAlunosPelaSituação.push(pegarAlunos.matricula)
-                listaAlunosPelaSituação.push(pegarAlunos.sexo)
-                status = true
-               }
-           })
-                
-                
-           
+    alunosJSON.forEach(function (pegarAlunos) {
+        pegarAlunos.curso.forEach(function (pegarCursos) {
+            pegarCursos.disciplinas.forEach(function (pegarStatus) {
+                //  const statusDoAluno =  pegarStatus.toLowerCase()
+                if (typeof pegarStatus.status === 'string' && pegarStatus.status.toLowerCase() === situação) {
+                    listaAlunosPelaSituação.push(pegarAlunos.nome)
+                    listaAlunosPelaSituação.push(pegarAlunos.foto)
+                    listaAlunosPelaSituação.push(pegarAlunos.matricula)
+                    listaAlunosPelaSituação.push(pegarAlunos.sexo)
+                    status = true
+                }
+            })
+
+
+
         })
     })
 
-    if(status){
+    if (status) {
         return listaAlunosPelaSituação
-    }else{
+    } else {
         return status = false
     }
 
-   
+
 }
-console.log(getAlunosStatus('aprovado'));
 
+function getCursoSigla(matricula) { // pega a média e as disciplina de um aluno pela matricula
+    let listaCursoSigla = [];
+    let listaMedia = [];
+    let status = false;
+    alunosJSON.forEach(function (pegarAlunos) {
+        pegarAlunos.curso.forEach(function (pegarCurso) {
+            pegarCurso.disciplinas.forEach(function (nomeCursoSigla) {
+                if (pegarAlunos.matricula == matricula) {
+                    let separar = "";
+                    const sigla = nomeCursoSigla.nome.split(" ");
+                    for (let i = 0; i < sigla.length; i++) {
+                        separar += sigla[i][0].toUpperCase();
+                    }
+                    listCursoSigla.push(separar);
+                    listaMedia.push(nomeCursoSigla.media);
 
+                    status = true;
+                }
+            });
+        });
+    });
 
+    if (status) {
+        return {
+            listCursoSigla,
+            listaMedia
+        }
+    } else {
+        return false;
+    }
+}
 
+module.exports = {
+    getCursos,
+    getTodosAlunos,
+    getAlunosPelaMatricula,
+    getAlunosPeloCurso,
+    getAlunosStatus,
+    getCursoSigla
+}
